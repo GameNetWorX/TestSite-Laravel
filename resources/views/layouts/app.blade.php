15c5a82cb106dcf869d272991aaaa16685c711d2
EuroCent <lenox82@googlemail.com>
2020-09-11 00:43:48 +0200

    2 statische Webseiten hinzugefügt

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item">
+                            <a href="/" class="nav-link {{ Request::is('/') ? 'active' : '' }}">Startseite</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/info" class="nav-link {{ Request::is('info') ? 'active' : '' }}">Information</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
